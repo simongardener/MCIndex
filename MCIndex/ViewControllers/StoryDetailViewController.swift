08edc1dda95875b5ad1cc065ff9b8dadd3bdff50
Simon Gardener <simongardener@me.com>
2019-02-26 22:48:16 +0000

    stack view at .6

--- MCIndex/ViewControllers/StoryDetailViewController.swift
+++ MCIndex/ViewControllers/StoryDetailViewController.swift
@@ -23,7 +23,6 @@ class StoryDetailViewController: UIViewController {
     @IBOutlet weak var redBlock: UIView!
     var story:Story!
     
-    @IBOutlet weak var slab: UILabel!
     
  
     override func viewDidLoad() {
@@ -37,7 +36,6 @@ class StoryDetailViewController: UIViewController {
     }
  
     func populateLabels(){
-        slab.text = story.title
         storyLabel.text = story.title
         thrillLabel.text = story.seriesName
         sourceLabel.text = story.issuesRun
